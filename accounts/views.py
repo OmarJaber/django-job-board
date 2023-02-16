fa45044f4f3fe1ec54c9c6f7940686bb47ae09f6
omarjaber <omar.ja93@gmail.com>
2023-02-16 16:15:19 +0200

    add signup page

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import SignUpForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+
 def signup(request):
     if request.method=='POST':
         form = SignUpForm(request.POST)
@@ -9,7 +11,9 @@ def signup(request):
             form.save()
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
-            user = authenticate(username, password)
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/accounts/profile')
     else:
         form = SignUpForm()
 
