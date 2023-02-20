8f6f2a58a044537c39352feac9f0d6b63ec26fe0
omarjaber <omar.ja93@gmail.com>
2023-02-20 23:04:13 +0200

    add links and login required

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.core.paginator import Paginator
+from django.urls import reverse
 
 # Create your views here.
+
+def send_message(request):
+    pass
+    # if request.method=='POST':
+    #     form = JobForm(request.POST, request.FILES)
+    #     if form.is_valid():
+    #         myform = form.save(commit=False)
+    #         myform.owner = request.user
+    #         myform.save()
+
+    #         return redirect(reverse('jobs:job_list'))
+    # else:
+    #     form = JobForm()
+
+    # context = {"form": form}
+
+    # return render(request, "contact/send_message.html", context)
+
+
+
+
