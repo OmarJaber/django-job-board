1ae8fc102e7f00d31f4e372a4cde50255bf3dd4c
omarjaber <omar.ja93@gmail.com>
2023-03-30 12:00:36 +0300

    django rest

--- job/api.py
+++ job/api.py
@@ -3,11 +3,29 @@ from .models import Job
 from .serializer import JobSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import generics
+
 
 @api_view(['GET'])
-def joblistapi(request):
+def job_list_api(request):
     all_jobs = Job.objects.all()
     data = JobSerializer(all_jobs, many=True).data
     return Response({'data': data})
+    
+@api_view(['GET'])
+def job_detail_api(request, id):
+    job_detail = Job.objects.get(id=id)
+    data = JobSerializer(job_detail).data
+    return Response({'data': data})
+
+
+class JobListApi(generics.ListCreateAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+
+class JobDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+    lookup_field = 'id'
     
     
